5a15cbce0369102df610e3795e48331007d15764
otherview <otherview@gmail.com>
2014-02-26 15:50:23 +0000

    Tree #3

--- API/Oreilly.py
+++ API/Oreilly.py
@@ -31,7 +31,7 @@ class Oreilly:
 
 
 # Returns the Pearson correlation coefficient for p1 and p2
-def sim_pearson(prefs,p1,p2):
+def sim_pearson_old(prefs,p1,p2):
   from math import sqrt
   # Get the list of mutually rated items
   si={}
@@ -65,6 +65,48 @@ def sim_pearson(prefs,p1,p2):
 
   return r
 
+# Returns the Pearson correlation coefficient for p1 and p2 from the slides
+def sim_pearson(prefs,p1,p2):
+  from math import sqrt
+  import numpy as np
+  # Get the list of mutually rated items
+  si={}
+  for item in prefs[p1]: 
+    if item in prefs[p2]: si[item]=1
+
+  # if they are no ratings in common, return 0
+  if len(si)==0: return 0
+
+  # Sum calculations
+  n=len(si)
+  
+  # Sums of all the preferences
+  sum1=sum([prefs[p1][it] for it in si])
+  sum2=sum([prefs[p2][it] for it in si])
+  
+  # Median of all preferences
+  med1 = sum1/len([prefs[p1][it] for it in si])
+  med2 = sum2/len([prefs[p2][it] for it in si])
+  
+  # Sum of the diference of item minus median times each other
+  toSum1 = sum([prefs[p1][it]-med1 for it in si])
+  toSum2 = sum([prefs[p2][it]-med2 for it in si])
+  totalDenominator = toSum1*toSum2
+ 
+  #End of denominator
+  #Start of ? numerator ?
+  # Sum of the products
+  toSum1 = sum(np.array([pow(prefs[p1][it]-med1,2) for it in si]))
+  toSum2 = sum(np.array([pow(prefs[p2][it]-med2,2) for it in si]))
+  
+  totalNumerator = sqrt(toSum1)*sqrt(toSum2)
+  if totalNumerator == 0:
+    return 0
+  pearson = totalDenominator / totalNumerator
+  
+  return pearson
+
+
 # Gets recommendations for a person by using a weighted average
 # of every other user's rankings
 def getRecommendations(prefs,person,similarity=sim_pearson):
@@ -78,7 +120,7 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
     # ignore scores of zero or lower
     if sim<=0: continue
     for item in prefs[other]:
-	    
+ 
       # only score movies I haven't seen yet
       if item not in prefs[person] or prefs[person][item]==0:
         # Similarity * Score
@@ -113,7 +155,7 @@ def simpleRecomendation():
   
   recomendationOreilly = Oreilly()
   
-  user = '0view'
+  user = 'dinojoe'
   testSubreddit = max(recomendationOreilly.dataset[user].iteritems(), key=operator.itemgetter(1))[0]
   
   
@@ -145,12 +187,50 @@ def recomendationAndEvaluation(times):
   return
   
   
+def recomendHowManyPosts(prefs,person,similarity=sim_pearson):
+  totals={}
+  simSums={}
+  sumPerson = sum([prefs[person][it] for it in si])
+  medPerson = sumPerson/len([prefs[person][it] for it in si])
+  
+  for other in prefs:
+    # don't compare me to myself
+    if other==person: continue
+    sim=similarity(prefs,person,other)
+    # Average for the other
+    sumOther = sum([prefs[other][it] for it in si])
+    medOther = sumOther/len([prefs[other][it] for it in si])
+    # ignore scores of zero or lower
+    if sim<=0: continue
+    for item in prefs[other]:
+ 
+      # only score movies I haven't seen yet
+      if item not in prefs[person] or prefs[person][item]==0:
+        # Similarity * Score
+        
+        
+	
+	totals.setdefault(item,0)
+        totals[item]+=prefs[other][item]*sim  #multiplica o que o outro acha pela similiaridade que tem com ele
+        # Sum of similarities
+        simSums.setdefault(item,0)
+        simSums[item]+=sim
+
+  # Create the normalized list
+  rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+  # Return the sorted list
+  rankings.sort()
+  rankings.reverse()
+  return rankings
+
   
 
 def main():
-  #simpleRecomendation()
-  for times in range(0,9):
-    recomendationAndEvaluation(times)
+  simpleRecomendation()
+  
+  #for times in range(0,9):
+  #  recomendationAndEvaluation(times)
     
   return 
 
